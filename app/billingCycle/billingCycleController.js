2f76cb098b9f40403138c93990bd2051b8b3472f
Unknown <joseeduardo@megasoftgyn.com.br>
2017-11-26 11:20:46 -0200

    Implantação do crud inicial

---
+++ app/billingCycle/billingCycleController.js
@@ -0,0 +1,63 @@
+(function(){
+  angular.module('primeiraApp').controller('BillingCycleCtrl', [
+    '$http',
+    'msgs',
+    'tabs',
+    BillingCycleController
+  ]);
+
+  function BillingCycleController($http, msgs, tabs) {
+    const vm = this;
+    const url = 'http://localhost:3003/api/billingCycles';
+
+    vm.refresh = function() {
+      $http.get(url).then(function(response) {
+        vm.billingCycle = {};
+        vm.billingCycles = response.data;
+        tabs.show(vm, {tabList: true, tabCreate: true});
+      })
+    }
+
+    vm.create = function() {
+      $http.post(url, vm.billingCycle).then(function(response) {
+        vm.refresh();
+        msgs.addSuccess('Operação realizada com sucesso!!');
+      }, function(response){
+        msgs.addError(response.data.errors);
+      });
+    }
+
+    vm.showTabUpdate = function(billingCycle) {
+      vm.billingCycle = billingCycle;
+      tabs.show(vm, {tabUpdate: true});
+    }
+
+    vm.showTabDelete = function(billingCycle) {
+      vm.billingCycle = billingCycle;
+      tabs.show(vm, {tabDelete: true});
+    }
+
+    vm.delete = function() {
+      const deleteUrl = `${url}/${vm.billingCycle._id}`
+      $http.delete(deleteUrl, vm.billingCycle).then(function success(response) {
+        vm.refresh();
+        msgs.addSuccess('Operação realizada com sucesso!');
+      }, function error(response) {
+        msgs.addError(response.data.errors);
+      });
+    }
+
+    vm.update = function() {
+      const updateUrl = `${url}/${vm.billingCycle._id}`;
+      $http.put(updateUrl, vm.billingCycle).then(function success(response){
+        vm.refresh();
+        msgs.addSuccess('Operação realizada com sucesso!');
+      }, function error(response) {
+        msgs.addError(response.data.errors);
+      })
+    }
+
+    vm.refresh();
+  }
+
+})()
--- app/config/routes.js
+++ app/config/routes.js
@@ -2,10 +2,12 @@ angular.module('primeiraApp').config([
   '$stateProvider',
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('dashboard', {
+    $stateProvider
+    .state('dashboard', {
       url: "/dashboard",
       templateUrl: "dashboard/dashboard.html"
-    }).state('billingCycle', {
+    })
+    .state('billingCycle', {
       url: "/billingCycle",
       templateUrl: "billingCycle/tabs.html"
     });
